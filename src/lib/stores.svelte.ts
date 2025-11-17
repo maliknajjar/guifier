2b68ba06ff0e630f2d9f8ad0d3f7336ae817ebce
malik najjar <malik446644@gmail.com>
2025-11-17 01:25:53 +0100

    progress

--- src/lib/stores.svelte.ts
+++ src/lib/stores.svelte.ts
@@ -9,12 +9,12 @@ export function setWebsiteColor(type: "json" | "yaml" | "xml" | "toml") {
         websiteStore.borderColor = "rgba(230, 230, 0, 1)";
     } else if (type === 'yaml') {
         websiteStore.backgroundColor = 'rgba(0, 0, 255, 0.25)';
-        websiteStore.borderColor = 'rgba(0, 0, 220, 0.4)';
+        websiteStore.borderColor = 'rgba(0, 0, 220, 0.15)';
     } else if (type === 'xml') {
         websiteStore.backgroundColor = 'rgba(255, 166, 0, 0.25)';
-        websiteStore.borderColor = 'rgba(230, 150, 0, 0.4)';
+        websiteStore.borderColor = 'rgba(230, 150, 0, 0.2)';
     } else if (type === 'toml') {
         websiteStore.backgroundColor = 'rgba(0, 128, 0, 0.25)';
-        websiteStore.borderColor = 'rgba(0, 110, 0, 0.4)';
+        websiteStore.borderColor = 'rgba(0, 110, 0, 0.15)';
     }
 }
